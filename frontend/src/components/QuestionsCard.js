45b7e4903a9c0ad2a45b0c0947cf7cc70a227d7d
Rickydlay <ehimwenmarichmich@gmail.com>
2025-07-09 14:22:59 +0100

    update frontend favicon

--- frontend/src/components/QuestionsCard.js
+++ frontend/src/components/QuestionsCard.js
@@ -6,7 +6,7 @@ const QuestionsCard = ({ question }) => {
   const excerpt = question.body.length > 100 ? `${question.body.substring(0, 100)}...` : question.body;
 
   return (
-    <Link to={`/questions/${question._id}`} className="card mb-3 text-decoration-none" style={{ width: '30%', margin: '0 15px', backgroundColor: '#ffffff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', transition: 'transform 0.2s', minHeight: '200px', flex: '1 0 auto' }} onClick={(e) => e.stopPropagation()}>
+    <Link to={`/questions/${question._id}`} className="card mb-3 text-decoration-none" style={{ width: '100%', margin: '10px', backgroundColor: '#ffffff', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', transition: 'transform 0.2s', minHeight: '200px', flex: '1 0 auto' }} onClick={(e) => e.stopPropagation()}>
       <div className="card-body" style={{ padding: '15px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
         <div>
           <h5 className="card-title" style={{ color: '#34495e', marginBottom: '10px', fontSize: '1.1rem' }}>
