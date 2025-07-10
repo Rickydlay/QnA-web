66df05dc83cd3a86b472307fbd410853dc1dd116
Rickydlay <ehimwenmarichmich@gmail.com>
2025-07-10 18:30:19 +0100

    fix: update baseURL in api.js for production

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL (e.g., your server address)
+  baseURL: 'https://qna-web-backend.onrender.com', // Replace with your backend URL (e.g., your server address)
   timeout: 5000, // Optional: set a timeout for requests
 });
 
